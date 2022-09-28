ec2656c8a077a6c0306fe5ef613af2978cc4168e
Cristian Tapia <cristianxd2022@gmail.com>
2022-09-27 22:42:43 -0300

    Combinación Shinigami - Cristian

---
+++ Biblioteca_de_Clases/Reserva.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_de_Clases
+{
+    public class Reserva
+    {
+        #region PROPIEDADES
+        public String Cod_reserva { get; set; }
+        public DateTime Fecha_hora { get; set; }
+        public String Motivo { get; set; }
+        public String Estado { get; set; }
+        public String Usuario_rut { get; set; }
+        public String Cod_tramite { get; set; }
+        #endregion
+
+        #region CONSTRUCTOR
+        public Reserva() //Inicializar Constructor
+        {
+            this.Init();
+        }
+
+        private void Init() //Constructor
+        {
+            Cod_reserva = String.Empty;
+            Fecha_hora = DateTime.Now;
+            Motivo = String.Empty;
+            Estado = String.Empty;
+            Usuario_rut = String.Empty;
+            Cod_reserva = String.Empty;
+        }
+        #endregion
+    }
+}
---
+++ Biblioteca_de_Clases/Tipo_tramite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca_de_Clases
+{
+    public class Tipo_tramite
+    {
+        #region PROPIEDADES
+        public String Cod_tramite { get; set; }
+        public String Nombre { get; set; }
+        public String Descripcion { get; set; }
+        public String Requisitos { get; set; }
+        public int Precio { get; set; }
+        public String Cod_template { get; set; }
+        #endregion
+
+        #region CONSTRUCTOR
+        public Tipo_tramite() //Inicializar Constructor
+        {
+            this.Init();
+        }
+
+        private void Init() // Constructor
+        {
+            Cod_tramite = String.Empty;
+            Nombre = String.Empty;
+            Descripcion = String.Empty;
+            Requisitos = String.Empty;
+            Precio = 0;
+            Cod_template = String.Empty;
+        }
+        #endregion
+    }
+}
