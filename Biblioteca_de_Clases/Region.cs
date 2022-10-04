a11a7f223efdee647531d9bfa98767f55c3fbb40
victor aedo <vict.aedo@duocuc.cl>
2022-10-03 22:17:00 -0300

    Revert "Integración Base de Datos SQLServer"
    This reverts commit e5c87f65e33fe19f27fe2f949b0a27346cdf836e.

--- Biblioteca_de_Clases/Region.cs
+++ Biblioteca_de_Clases/Region.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using Notaria.Datos;
-using System.Data.SqlClient;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Biblioteca_de_Clases
+namespace Biblioteca_clases
 {
     public class Region
     {
         #region PROPIEDADES
-        public string cod_region { get; set; }
-        public string nombre { get; set; }
+        public String Cod_region { get; set; }  
+        public String Nombre { get; set; }
         #endregion
 
         #region CONSTRUCTOR
@@ -23,37 +17,12 @@ namespace Biblioteca_de_Clases
 
         private void Init() //Constructor
         {
-            cod_region = string.Empty;
-            nombre = string.Empty;
+            Cod_region = String.Empty;
+            Nombre = String.Empty;
         }
         #endregion
 
         #region Metodos
-        public bool Create()
-        {
-            // Creo una instancia de conexión a Datos
-            //OnBreakEntities es el nombre de la conexión
-            PortafolioEntities bbdd = new PortafolioEntities();
-            Notaria.Datos.region regionOBJ = new Notaria.Datos.region();
-
-            try
-            {
-                //Pasar los valores de las propiedades de negocio a Datos
-                //CommonBC.Syncronize(desde, hasta); usa las propiedades definidas en this class
-                //luego se agraga con el metodo add con el objeto creado como parametro y saveChanges() guarda=Commit
-                CommonBC.Syncronize(this, regionOBJ);
-                bbdd.region.Add(regionOBJ);
-                bbdd.SaveChanges();
-                return true;
-            }
-            catch (Exception)
-            {
-                //En caso de que no se guarde, eliminara al objeto
-                bbdd.region.Remove(regionOBJ);
-                return false;
-            }
-
-        }
         #endregion
     }
 }
